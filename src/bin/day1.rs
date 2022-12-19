8e3829fa41e9e80191092f8e755dc5132c4afdc8
Eric Lavigne <hi_eric@hotmail.com>
2022-12-18 23:04:10 -0500

    Fix naming for day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,12 +1,15 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    let mut vals = include_str!("./p1-1.prod").split("\n\n").map(|elf| 
+    let mut vals = include_str!("./day1.prod").split("\n\n").map(|elf| 
         elf.split("\n").flat_map(|cal| cal.parse::<usize>()).sum::<usize>()
     ).collect::<Vec::<usize>>();
 
+    println!("Part 1 {:?}", vals.iter().max());
+
     vals.sort_by(|a, b| b.cmp(a));
 
-    println!("{:?}", vals.iter().take(3).sum::<usize>());
+    println!("Part 2 {:?}", vals.iter().take(3).sum::<usize>());
+    
     return Ok(());
 }
